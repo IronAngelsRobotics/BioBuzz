29d04452dd955c702dba42276df2c3c49856ed35
Mr.T <57153554+tnguyen2010@users.noreply.github.com>
2026-09-21 22:40:14 -0500

    Servo Demo
    * Initial Setup
    
    Created TeamCode36807 and demo OpMode
    
    * IronAngels_Android_Studio.pdf
    
    Instructions for Github and Android Studio
    
    * Servo Demo
    
    * Update MyServoDemo.java
    
    * minor spelling correction

---
+++ TeamCode36807/src/main/java/org/firstinspires/ftc/teamcode36807/OpModes/MyServoDemo.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode36807.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+/**
+ * Simple OpModes to control a servo.
+ */
+
+
+@TeleOp(name="MyServo")
+public class MyServoDemo extends LinearOpMode {
+    // Declare variables
+    Servo servo;
+
+    @Override
+    public void runOpMode() {
+        // Init hardware
+        servo = hardwareMap.get(Servo.class, "servo");
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Do something
+
+            if(gamepad1.y){
+                servo.setPosition(1);
+            }
+            if(gamepad1.x){
+                // reverse servo direction
+            }
+        }
+    }
+}
